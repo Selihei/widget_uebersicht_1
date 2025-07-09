c63a84d32ab58049bfaf7d4bd1feae9af0fd21a4
Selihei <selina.heinrichlp@gmail.com>
2025-07-09 12:44:56 +0200

    Creation and added some stuff

---
+++ README.md
@@ -0,0 +1,3 @@
+# widget_uebersicht_1
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(backgroundColor: Colors.blueAccent,centerTitle: true, title:  Text("Aufgabe 1")),
+       
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [ 
+            SizedBox(height: 55),
+            Text(' Hello App Akademie!', style: TextStyle(fontSize: 20,fontWeight: FontWeight.w900,color: Colors.lightBlue),),
+            SizedBox(height: 30),
+            Row(
+              children: [
+                SizedBox(width: 5),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.redAccent,
+                  child: Center(
+                    child: ElevatedButton(onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.purple,
+                      minimumSize: Size(0, 0),
+                      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 8),
+                    ),
+                    child: Text("A", style: TextStyle(color: Colors.white),))
+                  ),
+                ),
+                SizedBox(width: 8),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.green
+                ),
+                SizedBox(width: 8),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.blueAccent
+                ),
+              ],
+            )
+          ]
+        ),
+      ),
+    );
+  }
+}
